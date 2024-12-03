c60d42db97828165669961ca54cec253f605fd97
Adrián Romero <arr140@alu.ua.es>
2024-12-03 11:42:58 +0100

    Day1-2 finished

--- 1/day1.py
+++ 1/day1.py
@@ -1,24 +1,42 @@
-f = open("input.txt", "r")
-lines = f.readlines()
-f.close()
+def times_in_list(number, list):
+    count = 0
 
-list1 = []
-list2 = []
+    for i in list:
+        if i == number:
+            count += 1
 
-distance = 0
+    return count
 
-for line in lines:
-    separated_line = line.split("   ")
-    
-    list1.append(int(separated_line[0]))
-    list2.append(int(separated_line[1]))
+def main():
+    f = open("input.txt", "r")
+    lines = f.readlines()
+    f.close()
 
-# Ordenar las listas ascendentemente
-list1.sort()
-list2.sort()
+    list1 = []
+    list2 = []
 
-# Calcular la distancia
-for i in range(len(list1)):
-    distance += abs(list1[i] - list2[i])
+    distance = 0
+    similarity_score = 0
 
-print(distance)
+    for line in lines:
+        separated_line = line.split("   ")
+        
+        list1.append(int(separated_line[0]))
+        list2.append(int(separated_line[1]))
+
+    # Ordenar las listas ascendentemente
+    list1.sort()
+    list2.sort()
+
+    # Calcular la distancia
+    for i in range(len(list1)):
+        # distance += abs(list1[i] - list2[i])
+        score = times_in_list(list1[i], list2)
+        score = score * list1[i]
+        similarity_score += score
+
+    print(similarity_score)
+    #print(distance)
+
+if __name__ == "__main__":
+    main()
